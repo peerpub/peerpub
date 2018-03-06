d99adfb703952528268059e8df0bda4207b84451
Oliver Bertuch <o.bertuch@fz-juelich.de>
2018-03-06 21:05:00 +0100

    Add project Lombok for easier POJO code, that does not need special unit testing. * Add to Attribute.java:   * Getters + Setters   * ToString   * AllArgsConstructor

--- src/main/java/de/fzj/peerpub/model/doc/Attribute.java
+++ src/main/java/de/fzj/peerpub/model/doc/Attribute.java
@@ -1,9 +1,24 @@
 package de.fzj.peerpub.model.doc;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NonNull;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
+
+@ToString
+@AllArgsConstructor
 public class Attribute {
-  private String id;
-  private String name;
-  private String key;
-  private String description;
-  private String jsonschema;
+  @Getter @NonNull private final String id;
+
+  @NonNull @Getter @Setter private String name;
+  @NonNull @Getter @Setter private String key;
+  @NonNull @Getter @Setter private String description;
+
+  @NonNull @Getter private String jsonschema;
+  public void setJsonSchema(String schema) {
+    //TODO: Validation of the provided schema
+    //TODO: Add test
+    this.jsonschema = schema;
+  }
 }
