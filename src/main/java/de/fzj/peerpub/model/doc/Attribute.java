e015f7ff03803e709bd84c9458e09e1f1a429529
Oliver Bertuch <o.bertuch@fz-juelich.de>
2018-03-06 21:40:50 +0100

    Add Javadoc to Attribute.java

--- src/main/java/de/fzj/peerpub/model/doc/Attribute.java
+++ src/main/java/de/fzj/peerpub/model/doc/Attribute.java
@@ -9,16 +9,36 @@ import lombok.AllArgsConstructor;
 @ToString
 @AllArgsConstructor
 public class Attribute {
+  /**
+   * The attributes unique ID, will be used as primary key.
+   */
   @Getter @NonNull private final String id;
 
+  /**
+   * Display name for this attribute (printed as label)
+   */
   @NonNull @Getter @Setter private String name;
+  /**
+   * The key value, under which the Attribute value will be added to the
+   * composed metadata set.
+   */
   @NonNull @Getter @Setter private String key;
+  /**
+   * A short informative text presented to the user as a helping hand what
+   * this attribute is and what to insert.
+   */
   @NonNull @Getter @Setter private String description;
 
+  /**
+   * A JSON schema that allows the generation of input masks and
+   * validation of inserted data
+   */
   @NonNull @Getter private String jsonschema;
   public void setJsonSchema(String schema) {
     //TODO: Validation of the provided schema
     //TODO: Add test
     this.jsonschema = schema;
   }
+
+  //TODO: Future: add validation methods for data using the schema.
 }
