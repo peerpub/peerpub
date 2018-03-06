83ff3fe1ff3f2e320647557869aa26d3c64e3a08
Oliver Bertuch <o.bertuch@fz-juelich.de>
2018-03-06 19:09:25 +0100

    Remove Camunda BPM for now to focus on the basics for the web app.

--- src/main/java/de/fzj/peerpub/SingleComponentApplication.java
+++
@@ -1,9 +0,0 @@
-package de.fzj.peerpub;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Single Component App")
-public class SingleComponentApplication extends ServletProcessApplication {
-  // empty implementation
-}
