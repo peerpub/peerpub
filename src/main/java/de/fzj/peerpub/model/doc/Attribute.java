7bde0d099b7344b48b037050044ed158a6122c0e
Oliver Bertuch <o.bertuch@fz-juelich.de>
2018-03-06 20:58:06 +0100

    Introduce testing with JUnit 5 plus simple class model.doc.Attribute * Update pom.xml to include dependencies for JUnit 5 * Update pom.xml with configuration of Maven Plugin "Surefire" for unit testing * Fix indentation of pom.xml * Make easier versioning for dependencies via <properties>

---
+++ src/main/java/de/fzj/peerpub/model/doc/Attribute.java
@@ -0,0 +1,9 @@
+package de.fzj.peerpub.model.doc;
+
+public class Attribute {
+  private String id;
+  private String name;
+  private String key;
+  private String description;
+  private String jsonschema;
+}
---
+++ src/test/java/de/fzj/peerpub/model/doc/AttributeTest.java
@@ -0,0 +1,19 @@
+package de.fzj.peerpub.model.doc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+@Tag("fast")
+class AttributeTest {
+
+  @Test
+  @DisplayName("model.doc.Attribute validation test")
+  @Disabled("Future feature")
+  void validationTest(TestInfo testInfo) {}
+
+}
