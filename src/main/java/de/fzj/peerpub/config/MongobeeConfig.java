8e02437bed08da32c36443d4788e1b6024d35e99
Oliver Bertuch <o.bertuch@fz-juelich.de>
2018-03-27 00:39:28 +0200

    Refactor MongobeeConfig to be @SpringBootTest compatible.
    Running future integration tests fail as the Mongobee configuration
    was to static. It was not compatible with the usage of Embedded MongoDB
    and random port assigning, as the port were mapped hard from the config.
    
    By using @Autowire on MongoClient and MongoTemplate, the code is again
    loosely coupled and starts working when using an inside-box integration
    test with @SpringBootTest and @EnableAutoConfiguration to start the
    embedded MongoDB.

--- src/main/java/de/fzj/peerpub/config/MongobeeConfig.java
+++ src/main/java/de/fzj/peerpub/config/MongobeeConfig.java
@@ -2,25 +2,19 @@ package de.fzj.peerpub.config;
 
 import com.github.mongobee.Mongobee;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 /**
  * MongoBee configuration to execute database migrations when PeerPub is started
  */
 @Configuration
 @ComponentScan
-public class MongobeeConfig extends AbstractMongoConfiguration {
-  
-  /**
-   * MongoDB URI. Constructed from Spring Boot configuration values.
-   */
-  @Value("mongodb://${spring.data.mongodb.host}:${spring.data.mongodb.port}/${spring.data.mongodb.database}")
-  private String uri;
+public class MongobeeConfig {
   
   /**
    * MongoDB Database name. Taken from Spring Boot configuration value.
@@ -28,22 +22,25 @@ public class MongobeeConfig extends AbstractMongoConfiguration {
   @Value("${spring.data.mongodb.database}")
   private String database;
   
-  @Override
-  public MongoClient mongoClient() {
-    return new MongoClient(new MongoClientURI(uri));
-  }
+  /**
+   * MongoDB client, autowired in to allow Embedded MongoDB with random
+   * port to kick in.
+   */
+  @Autowired
+  private MongoClient mongoClient;
+  /**
+   * MongoDB template, autowired in to allow using Embedded MongoDB with
+   * the existing client connection.
+   */
+  @Autowired
+  private MongoTemplate mongoTemplate;
   
-  @Override
-  protected String getDatabaseName() {
-    return this.database;
-  }
-  
-  @Bean(name = "bee")
+  @Bean
   public Mongobee mongobee() throws Exception {
-    Mongobee runner = new Mongobee(uri);
-    runner.setDbName(getDatabaseName());
+    Mongobee runner = new Mongobee(mongoClient);
+    runner.setDbName(this.database);
     runner.setChangeLogsScanPackage("de.fzj.peerpub.config");
-    runner.setMongoTemplate(mongoTemplate());
+    runner.setMongoTemplate(mongoTemplate);
     return runner;
   }
 }
