1987ac3bc53106861dd1c8e75144c230ae329a33
Oliver Bertuch <o.bertuch@fz-juelich.de>
2018-04-09 11:13:06 +0200

    Refactor DocTypeService with explicit dependency on DocTypeRepositor

--- src/main/java/de/fzj/peerpub/doc/doctype/DocTypeService.java
+++ src/main/java/de/fzj/peerpub/doc/doctype/DocTypeService.java
@@ -3,7 +3,8 @@ package de.fzj.peerpub.doc.doctype;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import javax.validation.constraints.NotNull;
+import java.util.*;
 
 /**
  * Document Type Data Transfer Object
@@ -14,8 +15,15 @@ public class DocTypeService {
   /**
    * Document Type DAO
    */
-  @Autowired
   private DocTypeRepository docTypeRepository;
+  
+  /**
+   * Constructor to make explicit dependency on DocTypeRepository visible
+   * @param docTypeRepository
+   */
+  public DocTypeService(@Autowired DocTypeRepository docTypeRepository) {
+    this.docTypeRepository = docTypeRepository;
+  }
   
   /**
    * Get all document types and put into view model
